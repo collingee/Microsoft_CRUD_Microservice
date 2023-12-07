ac75e7549c601451afd70abccbe1d9584bdaff6c
collingee <collingee@ufl.edu>
2023-12-07 13:44:57 -0700

    Finished initial Employee Repository

--- src/main/java/com/collingee/microsoft_crud_microservice/employee/EmployeeRepository.java
+++ src/main/java/com/collingee/microsoft_crud_microservice/employee/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.collingee.microsoft_crud_microservice.employee;
 
-public interface EmployeeRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long>{
 
 } 
