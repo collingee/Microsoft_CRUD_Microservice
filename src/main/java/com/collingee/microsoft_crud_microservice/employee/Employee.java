d4ae939f6724d3762dc5813811fd2b96cf9c4d0f
collingee <collingee@ufl.edu>
2023-12-07 13:31:43 -0700

    Finished initial Employee Pojo

--- src/main/java/com/collingee/microsoft_crud_microservice/employee/Employee.java
+++ src/main/java/com/collingee/microsoft_crud_microservice/employee/Employee.java
@@ -1,8 +1,75 @@
+/**
+ * Author: Collin Gee
+ * Created: 12/7/2023
+ * Summary: This Pojo file contains the structure of the Employee class
+ **/
+
 package com.collingee.microsoft_crud_microservice.employee;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String department;
+    private boolean isActive;
+    private int salary;
+
+    Employee(){}
+
+    public String getEmployeeId(){
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeID){
+        this.employeeId = employeeID;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+    public boolean getIsActive(){
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive){
+        this.isActive = isActive;
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+
+    public void setSalary(int salary){
+        this.salary = salary;
+    }
 }
