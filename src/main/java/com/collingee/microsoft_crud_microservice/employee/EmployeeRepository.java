412d077caf7969fdf0ddae71da783e0ed7062619
collingee <collingee@ufl.edu>
2023-12-07 13:50:40 -0700

    Added comments to Employee Repository

--- src/main/java/com/collingee/microsoft_crud_microservice/employee/EmployeeRepository.java
+++ src/main/java/com/collingee/microsoft_crud_microservice/employee/EmployeeRepository.java
@@ -1,4 +1,10 @@
-package com.collingee.microsoft_crud_microservice.employee;
+/**
+ * Author: Collin Gee
+ * Created: 12/7/2023
+ * Summary: This file provides an interface for the 4 CRUD actions (create, read, update and delete)
+ **/
+
+ package com.collingee.microsoft_crud_microservice.employee;
 
 import org.springframework.data.repository.CrudRepository;
 
