d7e1d5c83fab10f69c61a0d9b68109b8c876825f
collingee <collingee@ufl.edu>
2023-12-08 10:22:00 -0700

    Moved Employee Repository

--- src/main/java/com/collingee/microsoft_crud_microservice/repository/EmployeeRepository.java
+++ src/main/java/com/collingee/microsoft_crud_microservice/repository/EmployeeRepository.java
@@ -4,9 +4,10 @@
  * Summary: This file provides an interface for the 4 CRUD actions (create, read, update and delete)
  **/
 
- package com.collingee.microsoft_crud_microservice.employee;
+ package com.collingee.microsoft_crud_microservice.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import com.collingee.microsoft_crud_microservice.entity.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long>{
 
