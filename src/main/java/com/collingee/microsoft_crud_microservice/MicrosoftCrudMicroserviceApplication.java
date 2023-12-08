5abef872da62f29778cd2e121f37306f48fa2b21
collingee <collingee@ufl.edu>
2023-12-08 10:34:54 -0700

    Created H2 Database in a file

--- src/main/java/com/collingee/microsoft_crud_microservice/MicrosoftCrudMicroserviceApplication.java
+++ src/main/java/com/collingee/microsoft_crud_microservice/MicrosoftCrudMicroserviceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MicrosoftCrudMicroserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MicrosoftCrudMicroserviceApplication.class, args);
 	}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+create table Employee(
+    employeeId bigint auto_increment,
+    firstName varchar(255) NOT NULL,
+    lastName varchar(255) NOT NULL,
+    department varchar(255) NOT NULL,
+    isActive boolean NOT NULL,
+    salary int varchar(255) NOT NULL
+);
