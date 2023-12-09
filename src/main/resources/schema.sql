aa7e85920e1f682cc354469be9ab3af16445b347
collingee <collingee@ufl.edu>
2023-12-08 17:57:00 -0700

    Added comments and cleaned code

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,9 @@
+/**
+ * Author: Collin Gee
+ * Created: 12/7/2023
+ * Summary: This file handles the creation of the Employee table in the database
+ **/
+
 create table Employee(
     employeeId bigint auto_increment,
     firstName varchar(255) NOT NULL,
